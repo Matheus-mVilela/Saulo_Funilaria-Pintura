e1afe0e80f2e73b78d8a6e1e0a7ef7b50dc3f7d6
Felipe <vilella.luisfelipe@gmail.com>
2020-05-27 11:33:01 -0300

    cool js carousel

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ $('.nt-albumDeFotos').magnificPopup({
     },
 });
 
+$("#team-members").owlCarousel({
+    items: 3,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items: 1
+        },
+        480:{
+            items: 2
+        },
+        768:{
+            items: 3
+        },
+    }
+});
