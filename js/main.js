eee5cd310020791b0ecf20d50ed007b05980d785
Matheus Vilela <matheus_vilela@icloud.com>
2020-06-09 17:23:18 -0300

    NavBar effect update

--- js/main.js
+++ js/main.js
@@ -29,15 +29,18 @@ const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll(){
-    const windowtop = window.pageYOffset;
+    const windowtop = window.pageYOffset; /* + ((window.innerHeight * 3) / 4) */
     target.forEach(function(element){
         if(windowtop > element.offsetTop){
             element.classList.add(animationClass);
+        } else{
+            element.classList.remove(animationClass);
         }
 
         console.log(element.offsetTop);
     })
 }
+
 
 window.addEventListener('scroll', function(){
     animeScroll();
