daf5713c86063fae5ea0057ee6c1feca291dda7d
Felipe <vilella.luisfelipe@gmail.com>
2020-06-11 13:32:56 -0300

    fixing some stuffs

---
+++ my_way_to_do_this/static/js/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+        }
+    });
+});
